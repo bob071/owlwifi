bbcfdc3a1897743ef21de89645bab14ae3529ae2
Bob Mulder <bob_071@live.nl>
2021-04-05 08:36:31 +0200

    Changes jeffrey

--- slider.js
+++ slider.js
@@ -6,109 +6,139 @@ gtag('js', new Date());
 gtag('config', 'UA-69056609-2');
 
 function init() {
-const sliders = document.getElementsByClassName("tick-slider-input");
+    const sliders = document.getElementsByClassName("tick-slider-input");
 
-for (let slider of sliders) {
-    slider.oninput = onSliderInput;
+    for (let slider of sliders) {
+        slider.oninput = onSliderInput;
 
-    updateValue(slider);
-    updateValuePosition(slider);
-    updateLabels(slider);
-    updateProgress(slider);
+        updateValue(slider);
+        updateValuePosition(slider);
+        updateLabels(slider);
+        updateProgress(slider);
 
-    setTicks(slider);
-}
+        setTicks(slider);
+    }
+
+    const checkboxes = document.getElementsByClassName("checkbox");
+    for (let checkbox of checkboxes) {
+        checkbox.oninput = updatePrice;
+    }
+
+    updatePrice();
 }
 
 function onSliderInput(event) {
-updateValue(event.target);
-updateValuePosition(event.target);
-updateLabels(event.target);
-updateProgress(event.target);
+    updateValue(event.target);
+    updateValuePosition(event.target);
+    updateLabels(event.target);
+    updateProgress(event.target);
+    updatePrice()
 }
 
 function updateValue(slider) {
-let value = document.getElementById(slider.dataset.valueId);
+    let value = document.getElementById(slider.dataset.valueId);
 
-value.innerHTML = "<div>" + slider.value + "</div>";
+    value.innerHTML = "<div>" + slider.value + "</div>";
+}
 
 // get slider.value om in een variabele op te slaan.
 // Mplaats in updateValue functie alle variable
 // If id = bewonersslider: then...
 
 function updateValuePosition(slider) {
-let value = document.getElementById(slider.dataset.valueId);
+    let value = document.getElementById(slider.dataset.valueId);
 
-const percent = getSliderPercent(slider);
+    const percent = getSliderPercent(slider);
 
-const sliderWidth = slider.getBoundingClientRect().width;
-const valueWidth = value.getBoundingClientRect().width;
-const handleSize = slider.dataset.handleSize;
+    const sliderWidth = slider.getBoundingClientRect().width;
+    const valueWidth = value.getBoundingClientRect().width;
+    const handleSize = slider.dataset.handleSize;
 
-let left = percent * (sliderWidth - handleSize) + handleSize / 2 - valueWidth / 2;
+    let left = percent * (sliderWidth - handleSize) + handleSize / 2 - valueWidth / 2;
 
-left = Math.min(left, sliderWidth - valueWidth);
-left = slider.value === slider.min ? 0 : left;
+    left = Math.min(left, sliderWidth - valueWidth);
+    left = slider.value === slider.min ? 0 : left;
 
-value.style.left = left + "px";
+    value.style.left = left + "px";
 }
 
 function updateLabels(slider) {
-const value = document.getElementById(slider.dataset.valueId);
-const minLabel = document.getElementById(slider.dataset.minLabelId);
-const maxLabel = document.getElementById(slider.dataset.maxLabelId);
+    const value = document.getElementById(slider.dataset.valueId);
+    const minLabel = document.getElementById(slider.dataset.minLabelId);
+    const maxLabel = document.getElementById(slider.dataset.maxLabelId);
 
-const valueRect = value.getBoundingClientRect();
-const minLabelRect = minLabel.getBoundingClientRect();
-const maxLabelRect = maxLabel.getBoundingClientRect();
+    const valueRect = value.getBoundingClientRect();
+    const minLabelRect = minLabel.getBoundingClientRect();
+    const maxLabelRect = maxLabel.getBoundingClientRect();
 
-const minLabelDelta = valueRect.left - (minLabelRect.left);
-const maxLabelDelta = maxLabelRect.left - valueRect.left;
+    const minLabelDelta = valueRect.left - (minLabelRect.left);
+    const maxLabelDelta = maxLabelRect.left - valueRect.left;
 
-const deltaThreshold = 32;
+    const deltaThreshold = 32;
 
-if (minLabelDelta < deltaThreshold) minLabel.classList.add("hidden");
-else minLabel.classList.remove("hidden");
+    if (minLabelDelta < deltaThreshold) minLabel.classList.add("hidden");
+    else minLabel.classList.remove("hidden");
 
-if (maxLabelDelta < deltaThreshold) maxLabel.classList.add("hidden");
-else maxLabel.classList.remove("hidden");
+    if (maxLabelDelta < deltaThreshold) maxLabel.classList.add("hidden");
+    else maxLabel.classList.remove("hidden");
 }
 
 function updateProgress(slider) {
-let progress = document.getElementById(slider.dataset.progressId);
-const percent = getSliderPercent(slider);
+    let progress = document.getElementById(slider.dataset.progressId);
+    const percent = getSliderPercent(slider);
 
-progress.style.width = percent * 100 + "%";
+    progress.style.width = percent * 100 + "%";
 }
 
 function getSliderPercent(slider) {
-const range = slider.max - slider.min;
-const absValue = slider.value - slider.min;
+    const range = slider.max - slider.min;
+    const absValue = slider.value - slider.min;
 
-return absValue / range;
+    return absValue / range;
 }
 
 function setTicks(slider) {
-let container = document.getElementById(slider.dataset.tickId);
-const spacing = parseFloat(slider.dataset.tickStep);
-const sliderRange = slider.max - slider.min;
-const tickCount = sliderRange / spacing + 1; // +1 to account for 0
+    let container = document.getElementById(slider.dataset.tickId);
+    const spacing = parseFloat(slider.dataset.tickStep);
+    const sliderRange = slider.max - slider.min;
+    const tickCount = sliderRange / spacing + 1; // +1 to account for 0
 
-for (let ii = 0; ii < tickCount; ii++) {
-    let tick = document.createElement("span");
+    for (let ii = 0; ii < tickCount; ii++) {
+        let tick = document.createElement("span");
 
-    tick.className = "tick-slider-tick";
+        tick.className = "tick-slider-tick";
 
-    container.appendChild(tick);
-}
+        container.appendChild(tick);
+    }
 }
 
 function onResize() {
-const sliders = document.getElementsByClassName("tick-slider-input");
+    const sliders = document.getElementsByClassName("tick-slider-input");
 
-for (let slider of sliders) {
-    updateValuePosition(slider);
+    for (let slider of sliders) {
+        updateValuePosition(slider);
+    }
 }
+
+function updatePrice() {
+    console.log("updateprice")
+    const bewoners = document.getElementById("bewonersSlider").value,
+          verdiepingen = document.getElementById("verdiepingenSlider").value,
+          lan = document.getElementById("lanSlider").value,
+          checkbox1 = document.getElementById("checkbox1").checked,
+          checkbox2 = document.getElementById("checkbox2").checked;
+
+    var price = bewoners * verdiepingen * lan;
+
+    if(checkbox1) {
+        price += 10
+    }
+
+    if(checkbox2) {
+        price -= 15
+    }
+
+    document.getElementById("totalprice").innerHTML = `&euro; ${price}`;
 }
 
 window.onload = init;
